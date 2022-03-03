2c6c62352e51113f1158cefdc4634248e575c6c1
Artur Ferreira Moreira <artur31415926@gmail.com>
2022-03-03 16:54:30 -0300

    player code updates

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from random import *
 
+from player import Player
+
 pygame.init()
 pygame.font.init()
 
@@ -21,6 +23,8 @@ running = True
 
 ticks = 0
 
+actual_player = Player((width / 2, height / 2))
+move_increment = 10
 ################################################################################################
 #                                           FUNCTIONS
 ################################################################################################
@@ -36,9 +40,22 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
-                print("pressed A")
+                actual_player.position[0] -= move_increment
             elif event.key == pygame.K_d:
-                print("pressed D")
+                actual_player.position[0] += move_increment
+
+            #TODO: IMPLEMENT ROTATION
+            if event.key == pygame.K_q:
+                pass
+            elif event.key == pygame.K_e:
+                pass
+
+            if event.key == pygame.K_SPACE:
+                #TODO: MAYBE CATCH MINERAL
+                pass
+            if event.key == pygame.K_f:
+                #TODO: MAYBE ATTACK
+                pass
 
 
     ##################################################################
@@ -47,7 +64,7 @@ while running:
     # Fill the background with white
     screen.fill((255, 255, 255))
 
-    
+    actual_player.draw(screen)
 
     textsurface = myfont.render('Ticks = ' + str(ticks), False, (0, 0, 0))
     screen.blit(textsurface, (0, 0))
--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+import math
 import pygame
 
 
@@ -5,6 +6,28 @@ class Player:
     def __init__(self, position) -> None:
         self.position = position
         self.width = 20
+    
+    def get_points(center, radius, mouse_position):
+        # calculate the normalized vector pointing from center to mouse_position
+        length = math.hypot(mouse_position[0] - center[0], mouse_position[1] - center[1])
+        # (note we only need the x component since y falls 
+        # out of the dot product, so we won't bother to calculate y)
+        angle_vector_x = (mouse_position[0] - center[0]) / length
+
+        # calculate the angle between that vector and the x axis vector (aka <1,0> or i)
+        angle = math.acos(angle_vector_x)
+
+        # list of un-rotated point locations
+        triangle = [0, (3 * math.pi / 4), (5 * math.pi / 4)]
+
+        result = list()
+        for t in triangle:
+            # apply the circle formula
+            x = center[0] + radius * math.cos(t + angle)
+            y = center[1] + radius * math.sin(t + angle)
+            result.append((x, y))
+
+        return result
 
     def draw(self, surface):
         top_p = (self.position.x + self.width / 2, self.position.y)
